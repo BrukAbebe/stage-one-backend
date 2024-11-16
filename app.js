b73304eb8a1a5159d6a4e0333d8a20a4f223a4a7
BrukAbebe <birukgalk@gmail.com>
2024-11-16 19:08:30 +0300

    Add /hobby route returning a JSON object

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const nameRoute = require('./routes/name');
+const nameRoute = require('./routes/nameRoute');
+const hobbyRoute = require('./routes/hobbyRoute');
 
 const app = express();
 const PORT = 3000;
 
 app.use('/name', nameRoute);
+app.use('/hobby', hobbyRoute);
+
 
 
 app.listen(PORT, () => {
