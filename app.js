5c9feb6ce985e957ea7953e39dc777b5d3ae80b0
BrukAbebe <birukgalk@gmail.com>
2024-11-16 17:21:06 +0300

    Add /name route returning full name

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/name', (req, res) => {
+    res.status(200).send('Biruk Abebe');
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on :${PORT}`);
